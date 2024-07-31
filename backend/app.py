626f9dab9791e320da3440d56995c37278c52e25
ReneSanchezH <rene.sanchez2001@alumnos.ubiobio.cl>
2024-07-31 00:22:28 -0400

    actualización verificación palabras

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,8 @@ from flask import Flask, request, jsonify, send_from_directory
 import os
 from manim import config
 from radix_sort_scene import RadixSort
-from radix_detailed_scene import RadixDetailed  # Importar escena detallada
-from counting_sort_scene import CountingSort  # Importar escena de Counting Sort
-from bucket_scene import Bucket  # Importar escena de Bucket
 from flask_cors import CORS
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.data import find
+import spacy
 
 app = Flask(__name__)
 CORS(app)
@@ -17,18 +12,19 @@ output_dir = os.path.join(os.path.dirname(__file__), 'videos')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Verificar y descargar recursos de nltk si es necesario
-try:
-    find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+# Cargar el modelo de lenguaje de spaCy
+nlp = spacy.load("en_core_web_sm")
 
 def should_generate_detailed_video(prompt):
     detailed_keywords = [
-        "detallado", "detalles", "explicaciones", "explicación",  "analítico", "minucioso", "paso a paso", "detalladamente", "explicación completa", "en profundidad", "descripción detallada", "en detalle", "con detalle", "explicación exhaustiva", "detallada explicación", "en detalle", "paso por paso", "detallada", "completamente", "explicaciones detalladas", "descripción en detalle", "información completa", "desglose detallado"
+        "detallado", "detalles", "explicaciones", "explicación", "analítico", "minucioso", "paso a paso", 
+        "detalladamente", "explicación completa", "en profundidad", "descripción detallada", "en detalle", 
+        "con detalle", "explicación exhaustiva", "detallada explicación", "en detalle", "paso por paso", 
+        "detallada", "completamente", "explicaciones detalladas", "descripción en detalle", "información completa", 
+        "desglose detallado"
     ]
-    tokens = word_tokenize(prompt.lower())
-    return any(word in tokens for word in detailed_keywords)
+    doc = nlp(prompt.lower())
+    return any(token.text in detailed_keywords for token in doc)
 
 def should_generate_counting_sort_video(prompt):
     counting_sort_keywords = [
@@ -39,8 +35,8 @@ def should_generate_counting_sort_video(prompt):
         "counting sort explicado", "counting sort detallado", "contar y ordenar", "explicación de counting sort",
         "ordenación por contaje", "ordenar mediante conteo", "sort basado en conteo", "explicación exhaustiva counting sort"
     ]
-    tokens = word_tokenize(prompt.lower())
-    return any(word in tokens for word in counting_sort_keywords)
+    doc = nlp(prompt.lower())
+    return any(token.text in counting_sort_keywords for token in doc)
 
 def should_generate_bucket_sort_video(prompt):
     bucket_sort_keywords = [
@@ -51,15 +47,17 @@ def should_generate_bucket_sort_video(prompt):
         "bucket sort explicado", "bucket sort detallado", "cubetas de ordenamiento", "explicación de bucket sort",
         "ordenación mediante cubetas", "bucket sort detalladamente"
     ]
-    tokens = word_tokenize(prompt.lower())
-    return any(word in tokens for word in bucket_sort_keywords)
+    doc = nlp(prompt.lower())
+    return any(token.text in bucket_sort_keywords for token in doc)
 
 @app.route('/generate-video', methods=['POST'])
 def generate_video():
     try:
         data = request.json
         numbers_string = data.get('numbers')
-        prompt = data.get('prompt')  # Añadir el prompt al payload de la solicitud
+        prompt = data.get('prompt')
+        print("prompt", prompt)
+        print("numbers_string", numbers_string)
 
         if not numbers_string or not isinstance(numbers_string, str):
             return jsonify({"error": "Invalid input format"}), 400
@@ -70,7 +68,7 @@ def generate_video():
             formatted_string = ''.join(numbers_string.split(','))
         except ValueError:
             return jsonify({"error": "Invalid input format"}), 400
-        
+
         config.media_dir = output_dir
 
         # Definir rutas de salida para cada tipo de video
@@ -79,22 +77,15 @@ def generate_video():
         bucket_sort_output_file = os.path.join(output_dir, 'bucket.mp4')
         dynamic_output_file = os.path.join(output_dir, f'radix_sort_{formatted_string}.mp4')
 
+        # Verificar si se debe usar un video estático
         if should_generate_detailed_video(prompt):
-            if not os.path.exists(detailed_output_file):
-                scene = RadixDetailed(numbers)
-                scene.render()
             video_url = '/videos/radix_detailed.mp4'
         elif should_generate_counting_sort_video(prompt):
-            if not os.path.exists(counting_sort_output_file):
-                scene = CountingSort(numbers)
-                scene.render()
             video_url = '/videos/counting_sort.mp4'
         elif should_generate_bucket_sort_video(prompt):
-            if not os.path.exists(bucket_sort_output_file):
-                scene = Bucket()
-                scene.render()
             video_url = '/videos/bucket.mp4'
         else:
+            # Generar dinámicamente el video de Radix Sort si no se encontraron palabras clave estáticas
             if not os.path.exists(dynamic_output_file):
                 class RadixSortSceneWrapper(RadixSort):
                     def __init__(self, **kwargs):
